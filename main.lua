5c719508056f0d63390d7d6bacebc00067d2a878
Joel Singh <joelsingh788@gmail.com>
2024-05-27 18:12:35 -0400

    Little guy is moving

--- main.lua
+++ main.lua
@@ -12,9 +12,15 @@ function love.load()
   player.y = love.graphics.getHeight() / 2
 
   player.img = love.graphics.newImage('my-little-guy.png')
+  player.speed = 200
 end
 
 function love.update(dt)
+  if love.keyboard.isDown('d') then
+    player.x = player.x + (player.speed * dt)
+  elseif love.keyboard.isDown('a') then
+    player.x = player.x - (player.speed * dt)
+  end
 end
 
 function love.draw()
