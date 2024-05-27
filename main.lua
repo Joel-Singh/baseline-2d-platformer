2e26908cc02c952e58b487045010c2a1f3d3ee29
Joel Singh <joelsingh788@gmail.com>
2024-05-27 18:03:36 -0400

    Little guy is showing up

--- main.lua
+++ main.lua
@@ -7,6 +7,11 @@ function love.load()
 
   platform.x = 0
   platform.y = platform.height / 2
+
+  player.x = love.graphics.getWidth() / 2
+  player.y = love.graphics.getHeight() / 2
+
+  player.img = love.graphics.newImage('my-little-guy.png')
 end
 
 function love.update(dt)
@@ -18,4 +23,6 @@ function love.draw()
   love.graphics.rectangle(
     'fill', platform.x, platform.y, platform.width, platform.height
   )
+
+  love.graphics.draw(player.img, player.x, player.y, 0, 1, 1, 0, 32)
 end
